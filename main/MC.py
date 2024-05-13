40ee4290951f00625bab04929bca1dd8b7b066d1
ZeroKong413 <asdfzxcv5187@gmail.com>
2024-05-13 17:11:23 +0900

    0513

--- main/MC.py
+++ main/MC.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 url = 'http://127.0.0.1:5000/send_data'
-data = {'MBTI':'ESFP', "age":"24"}  # 보낼 데이터를 딕셔너리로 정의
+data = {'age':'24', "name":"Kim"}  # 보낼 데이터를 딕셔너리로 정의
 
 response = requests.post(url, json=data)
 print(response.text)  # 서버로부터 받은 응답 출력
